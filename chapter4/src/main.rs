4a1909ba9b7263db9d3ffcd7a5586b98010284f8
ecly <elyne@live.dk>
2016-06-04 21:09:54 +0200

    chapter 4.3

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -1,4 +1,34 @@
 fn main() {
-    let x: i32;
-    println!("The value of x is: {}", x);
+    println!("x is: {}", return_number (32));
+}
+
+fn types(){
+    //slices
+    let a = [0, 1, 2, 3, 4];
+    let complete = &a[..]; // A slice containing all of the elements in a
+    let middle = &a[1..4]; // A slice of a: only the elements 1, 2, and 3
+    let x: (i32, &str) = (1, "hello");
+}
+
+fn deconstruction(){
+    let (x, y, z) = (1, 2, 3);
+    println!("x is {}", x);
+}
+
+fn tuples(){
+    (0,); // single-element tuple
+    (0); // zero in parentheses
+    let tuple = (1, 2, 3);
+
+    let x = tuple.0;//tuple referencing uses . rather than []
+    let y = tuple.1;
+    let z = tuple.2;
+}
+
+fn function_types(){
+    fn foo(x: i32) -> i32 { x }
+    let x: fn(i32) -> i32 = foo;//rebinding with no type inference
+}
+fn return_number(x: i32) -> i32 {
+    x + 1
 }

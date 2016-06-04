696201f964cb6e4535b04dce6a0e44431e4a1aae
ecly <elyne@live.dk>
2016-06-04 22:06:59 +0200

    some comments on documentation

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -1,8 +1,26 @@
+/// rustdoc can be used to compile HTML documentation
+/// using the doc comments
 fn main() {
     println!("x is: {}", return_number (32));
 }
 
-fn types(){
+/// Doc commenting
+///
+/// # Examples
+///
+/// ```
+/// let five = 5;
+///
+/// assert_eq!(6, add_one(5));
+/// # fn add_one(x: i32) -> i32 {
+/// #     x + 1
+/// # }
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn types() {
     //slices
     let a = [0, 1, 2, 3, 4];
     let complete = &a[..]; // A slice containing all of the elements in a
@@ -10,12 +28,12 @@ fn types(){
     let x: (i32, &str) = (1, "hello");
 }
 
-fn deconstruction(){
+fn deconstruction() {
     let (x, y, z) = (1, 2, 3);
     println!("x is {}", x);
 }
 
-fn tuples(){
+fn tuples() {
     (0,); // single-element tuple
     (0); // zero in parentheses
     let tuple = (1, 2, 3);
@@ -25,7 +43,7 @@ fn tuples(){
     let z = tuple.2;
 }
 
-fn function_types(){
+fn function_types() {
     fn foo(x: i32) -> i32 { x }
     let x: fn(i32) -> i32 = foo;//rebinding with no type inference
 }
